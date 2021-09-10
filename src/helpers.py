4907425d6c1de2013b05ebcb9799587af84a63e7
gomonuk <gomoniukde@biocad.ru>
2021-09-10 21:19:10 +0300

    init

---
+++ src/helpers.py
@@ -0,0 +1,6 @@
+def check(in_str: str) -> bool:
+    return bool(in_str)
+
+
+def calc(string: str) -> str:
+    return eval(string)
---
+++ src/main.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from src.helpers import check, calc
+
+TOKEN = "1970454158:AAGPh2dEwz1Z18p6EoPDQnyZlGRwTN1l5l8"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start", "help"])
+async def send_welcome(msg: types.Message):
+    await msg.reply(f"Я бот. Приятно познакомиться,{msg.from_user.first_name}")
+
+
+@dp.message_handler(content_types=["text"])
+async def get_text_messages(msg: types.Message):
+    answer = "Не работает"
+    if check(msg.text.lower()):
+        answer = calc(msg.text.lower())
+
+    await msg.answer(answer)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
